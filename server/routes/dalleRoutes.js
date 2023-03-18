c6f8b2703b9ba83df346f4b8784cc51aaab39f6c
LukasFrogner <lukasfrognerjobb@gmail.com>
2023-03-18 12:10:05 +0100

    vite config

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,37 +1,42 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import { v2 as cloudinary } from 'cloudinary';
+
+import Post from '../mongodb/models/post.js';
 
 dotenv.config();
 
 const router = express.Router();
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const openai = new OpenAIApi(configuration);
-
-router.route('/').get((req, res) => {
-  res.status(200).json({ message: 'Hello from DALL-E!' });
+router.route('/').get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+  }
 });
 
 router.route('/').post(async (req, res) => {
   try {
-    const { prompt } = req.body;
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
 
-    const aiResponse = await openai.createImage({
+    const newPost = await Post.create({
+      name,
       prompt,
-      n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json',
+      photo: photoUrl.url,
     });
 
-    const image = aiResponse.data.data[0].b64_json;
-    res.status(200).json({ photo: image });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
   }
 });
 
